7221f0b3a28e17da7b9365f9591176986cae1b4b
Martin Sundhaug <martinsundhaug@gmail.com>
2015-04-07 12:57:43 +0200

    pers: fc should be 4, not 2

--- main.py
+++ main.py
@@ -132,8 +132,8 @@ nmtECData[:]=[5,0,0,0,0,0,0,0]
 pdo1Data=(c_uint8*8)()
 pdo1Data[:]=[0xdb,0x11,0x64,1,0,0,0,0]
 personality=[CANopenFrame(function_code=0xe,data=CANopenPayload(data=nmtECData),data_len=1),
-             CANopenFrame(function_code=0x2,data=CANopenPayload(data=pdo1Data),data_len=8),
-             CANopenFrame(function_code=0x2,data=CANopenPayload(data=pdo1Data),data_len=8)]
+             CANopenFrame(function_code=0x4,data=CANopenPayload(data=pdo1Data),data_len=8),
+             CANopenFrame(function_code=0x4,data=CANopenPayload(data=pdo1Data),data_len=8)]
 personalityLengths=[2,3]
 print "Config",config
 
