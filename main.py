2eb25beb3cda84744c5b5f31d830956f988e5c4c
Martin Sundhaug <martinsundhaug@gmail.com>
2015-05-02 11:11:55 +0200

    rootCA.pem use relative path and add more spacing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pycanopen import *
 from datetime import *
 from urllib2 import *
 from argparse import *
-import urllib,hmac,hashlib
+import urllib, hmac, hashlib
 
 def parse16(array, offset):
     return (array[offset + 1] << 8) + array[offset]
@@ -35,76 +35,78 @@ def ArrayToCuint8(array):
 
 def handleTimeStampMsg(pkg):
     if pkg.data_len<>6:
-        sendError(config.company,config.ship,config.controller,config.instance,"Timestamp data length error")
+        sendError(config.company, config.ship, config.controller, config.instance, "Timestamp data length error")
         return
     dayL = list(pkg.data.data)
     daysSince84 = parse16(dayL, 4)
 
     global prevDS84, prevMsm
     msSinceMidnight = parse32(pkg.data.data, 0)
-    print daysSince84,msSinceMidnight
+    print daysSince84, msSinceMidnight
 
     if prevDS84 <> 0:
         if daysSince84 < prevDS84:
-            sendError(config.company,config.ship,config.controller,config.instance,"Timestamp days reduced #nodelorean")
+            sendError(config.company, config.ship, config.controller, config.instance, "Timestamp days reduced #nodelorean")
             return
         if daysSince84 > prevDS84 + 1:
-            sendError(config.company,config.ship,config.controller,config.instance,"Timestamp day skipped #nodelorean")
+            sendError(config.company, config.ship, config.controller, config.instance, "Timestamp day skipped #nodelorean")
             return
         global prevMsm
         if msSinceMidnight < prevMsm and daysSince84 == prevDS84:
-            sendError(config.company,config.ship,config.controller,config.instance,"Timestamp millis reduced without day-change #nodelorean")
+            sendError(config.company, config.ship, config.controller, config.instance, "Timestamp millis reduced without day-change #nodelorean")
             return
         if msSinceMidnight == prevMsm and daysSince84 == prevDS84:
-            sendError(config.company,config.ship,config.controller,config.instance,"Timestamp duplicated frame")
+            sendError(config.company, config.ship, config.controller, config.instance, "Timestamp duplicated frame")
             return        
 
     prevDS84=daysSince84
     prevMsm=msSinceMidnight
     
 
-    sendAlive(config.company,config.ship,config.controller,config.instance,daysSince84,msSinceMidnight)
+    sendAlive(config.company, config.ship, config.controller, config.instance, daysSince84, msSinceMidnight)
 
-def httpGet(url,sign=True):
+def httpGet(url, sign = True):
     try:
         if sign:
             global signature
             signature=hmac.new(config.privatekey, signature, hashlib.sha512).hexdigest()
             url+='&signature='+signature
-        print '<',url
-        resp=urlopen(url,cafile="/home/pi/certificate/rootCA.pem")
+        print '<', url
+        resp=urlopen(url, cafile="../certificate/rootCA.pem")
         str=resp.read()
-        print '> ',str
+        print '> ', str
         resp.close()
         return True
     except URLError as error:
-        print 'URLError',error,'requesting',url
+        print 'URLError', error, 'requesting', url
         return False
 
-def sendAlive(company,ship,controller,instance,day,ms):
+def sendAlive(company, ship, controller, instance, day, ms):
     if not errorReportFailed:
         url=config.remotescheme+'://'+config.remotehost+':'+config.remoteport+'/Alive?company='+str(company)+'&ship='+str(ship)+'&controller='+str(controller)+'&instance='+str(instance)+'&day='+str(day)+'&ms='+str(ms)
         if not httpGet(url):
-            sendError(company,ship,controller,instance,"One or more alive-reports failed")
+            sendError(company, ship, controller, instance, "One or more alive-reports failed")
     else:
-        sendError(company,ship,controller,instance,"One or more error-reports failed")
+        sendError(company, ship, controller, instance, "One or more error-reports failed")
 
-def sendReset(company,ship,controller,instance):
+def sendReset(company, ship, controller, instance):
     if not errorReportFailed:
         url=config.remotescheme+'://'+config.remotehost+':'+config.remoteport+'/Alert?company='+str(company)+'&ship='+str(ship)+'&controller='+str(controller)+'&instance='+str(instance)+'&error=reset'
         if not httpGet(url):
-            sendError(company,ship,controller,instance,"Unable to reset")
+            sendError(company, ship, controller, instance, "Unable to reset")
     else:
-        sendError(company,ship,controller,instance,"One or more error-reports failed")
+        sendError(company, ship, controller, instance, "One or more error-reports failed")
 
-def sendError(company,ship,controller,instance,error):
+def sendError(company, ship, controller, instance, error):
     global errorReportFailed
     errorReportFailed=True
     url=config.remotescheme+'://'+config.remotehost+':'+config.remoteport+'/Alert?company='+str(company)+'&ship='+str(ship)+'&controller='+str(controller)+'&instance='+str(instance)+'&error='+urllib.quote_plus(error)
     errorReportFailed=not httpGet(url)
-def checkNodeIdIsMine(frame,nodeid):
+
+def checkNodeIdIsMine(frame, nodeid):
     return int(frame.id & 127) == nodeid
-def checkNmtRegardsMe(frame,nodeid):
+
+def checkNmtRegardsMe(frame, nodeid):
     if frame.function_code<>0 or int(frame.id&127) <> 0:
         return False
     return int(frame.data.data[1])&127==nodeid&127
@@ -129,23 +131,24 @@ parser.add_argument("--signature",default="Never gonna let you down")
 
 config=parser.parse_args()
 
-errorReportFailed,prevDS84,prevMsm,signature,previousPkgSendTime,max,pos,lfsSeenCount,lastFrameSent=0,0,0,config.signature,0,0,0,0,0
+errorReportFailed, prevDS84, prevMsm, signature, previousPkgSendTime, max, pos, lfsSeenCount, lastFrameSent = 0, 0, 0, config.signature, 0, 0, 0, 0, 0
 
 nmtECData=(c_uint8*8)()
-nmtECData[:]=[5,0,0,0,0,0,0,0]
+nmtECData[:]=[5, 0, 0, 0, 0, 0, 0, 0]
 pdo1Data=(c_uint8*8)()
-pdo1Data[:]=[0xdb,0x11,0x64,1,0,0,0,0]
-personality=[CANopenFrame(function_code=0xe,data=CANopenPayload(data=nmtECData),data_len=1),
-             CANopenFrame(function_code=0x4,data=CANopenPayload(data=pdo1Data),data_len=8),
-             CANopenFrame(function_code=0x4,data=CANopenPayload(data=pdo1Data),data_len=8)]
-personalityLengths=[2,3]
-print "Config",config
+pdo1Data[:]=[0xdb, 0x11, 0x64, 1, 0, 0, 0, 0]
+personality=[CANopenFrame(function_code = 0xe, data = CANopenPayload(data = nmtECData), data_len = 1),
+             CANopenFrame(function_code = 0x4, data = CANopenPayload(data = pdo1Data), data_len = 8),
+             CANopenFrame(function_code = 0x4, data = CANopenPayload(data = pdo1Data), data_len = 8)]
+personalityLengths=[2, 3]
+
+print "Config", config
 
 inputBus=CANopen(config.input)
 outputBus=CANopen(config.output)
-print "inputBus",inputBus,"outputBus",outputBus
+print "inputBus", inputBus, "outputBus", outputBus
 
-sendReset(config.company,config.ship,config.controller,config.instance) # Bootup message
+sendReset(config.company, config.ship, config.controller, config.instance) # Bootup message
 while True:
     if (max not in range(len(personality))) or not pos in range(max):
         pos=0
@@ -166,6 +169,7 @@ while True:
         frame = inputBus.read_frame()
     except Exception, inst:
         print ('Exception in read_frame:', inst.args)
+        sys.exit(-1)
     if frame:
         if int(frame.id & 127) == 0 and frame.function_code == 2:
             handleTimeStampMsg(frame)
@@ -173,10 +177,10 @@ while True:
             lfsSeenCount+=1
         if (frame == lastFrameSent and lfsSeenCount>1) or \
          (frame<>lastFrameSent and 
-          (checkNodeIdIsMine(frame,config.nodeid) or
-           checkNmtRegardsMe(frame,config.nodeid))):
-            print 'ALERT', frame,frame == lastFrameSent,checkNodeIdIsMine(frame,config.nodeid)
-            sendError(config.company,config.ship,config.controller,config.instance,'MJOW')
+          (checkNodeIdIsMine(frame, config.nodeid) or
+           checkNmtRegardsMe(frame, config.nodeid))):
+            print 'ALERT', frame, frame == lastFrameSent, checkNodeIdIsMine(frame, config.nodeid)
+            sendError(config.company, config.ship, config.controller, config.instance, 'MJOW')
 			
 
 
